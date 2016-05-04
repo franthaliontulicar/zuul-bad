79383c70f7c2ddf2e496e8373fcbf34542d8315e
paco <franmenendezpouso@gmail.com>
2016-05-04 21:50:41 +0200

    arreglado bug en la clase game que te pedia el comando de ayuda en ingles y no en italiano

--- Game.java
+++ Game.java
@@ -107,7 +107,7 @@ public class Game
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type 'help' if you need help.");
+        System.out.println("Escribe aiuto si necesitas ayuda.");
         System.out.println();
         System.out.println("You are " + currentRoom.getDescription());
         System.out.print("Exits: ");
