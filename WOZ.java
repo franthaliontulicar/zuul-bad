00f46f93fd6e0a7ed7df71c0fdcce15dd0bdf9c7
paco <franmenendezpouso@gmail.com>
2016-05-20 09:39:43 +0200

    añadida clase aplicacion

---
+++ WOZ.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class WOZ here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WOZ
+{
+    // instance variables - replace the example below with your own
+   private static Game juego;
+ 
+     public static void main(String[] args) {
+         juego = new Game();
+         juego.play();
+     }
+}
